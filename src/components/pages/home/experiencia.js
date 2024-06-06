f91ea1facd0f2c744c3aa213ad83f0c4206bc47e
jazdian <jazdian@gmail.com>
2024-06-06 00:47:37 -0600

    se actualiza modulo de experiencia

--- src/components/pages/home/experiencia.js
+++ src/components/pages/home/experiencia.js
@@ -1,17 +1,108 @@
 export function loadExperiencia() {
     const content = /* html */
     `
-    <section id="experiencia" class="my-8 container mx-auto mt-10">
-      <h2 class="text-3xl font-semibold">Experiencia</h2>
-
+    <section id="experiencia" class="my-8 container gradiente3 mx-auto mt-5">
+      
       <div class="flex flex-row">
-        <img src="assets/images/Imagen_RGC.png" class="rounded-lg w-44 h-64 mr-4">
-        <div class="w-full aspect-video">
-          <p>Ut duis est ipsum do velit irure do cupidatat eu qui exercitation sint officia. Proident ad occaecat qui laboris quis veniam enim eiusmod mollit sunt irure ut. Nulla minim in velit dolore veniam occaecat officia magna dolore nostrud magna aliquip.</p>
-          <p>In fugiat ut ea adipisicing. Ipsum et proident reprehenderit laboris eu. Voluptate et nisi nostrud minim dolor ipsum.
-          <p>Proident fugiat est dolore labore esse sunt culpa ullamco et aliquip tempor. Voluptate sint ex ex aute sunt in fugiat. Cillum consectetur minim laboris enim. Aute et esse ut cillum Lorem et.</p>
-          <p>Tempor ullamco laboris ullamco cillum non irure culpa duis ad irure eu anim. Culpa veniam voluptate irure proident. Est Lorem dolor nisi aute quis officia culpa est laboris. Labore sit deserunt ex consectetur pariatur dolor dolore laboris amet. Ea reprehenderit officia ea excepteur consectetur Lorem ullamco deserunt anim proident ad exercitation mollit.</p>
-          <p>Proident fugiat est dolore labore esse sunt culpa ullamco et aliquip tempor. Voluptate sint ex ex aute sunt in fugiat. Cillum consectetur minim laboris enim. Aute et esse ut cillum Lorem et.</p>
+      
+        <div class="container mx-auto p-4">
+          <h2 class="text-3xl ubuntu-sans-mono-code text-center mb-8">
+            Experiencia
+          </h2>
+          <div class="flex items-center justify-center">
+            <div class="text-center">
+              <img src="assets/images/Imagen_RGC.png" class="w-40 h-64 mr-4 rounded-full mx-auto shadow-lg">
+            </div>
+          </div>
+          <div class="relative">
+            <!-- Línea central -->
+            <div class="absolute left-1/2 transform -translate-x-1/2 h-full border-l-2 border-gray-300"></div>
+    
+            <!-- Evento 1 -->
+            <div class="mb-8 flex justify-between items-center w-full">
+              <div class="order-1 w-5/12"></div>
+              <div class="z-20 flex items-center order-1 soft-glass-ui shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">1</h1>
+              </div>
+              <div class="order-1 soft-glass-ui rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-3 ubuntu-sans-mono-code font-bold text-white text-xl">1997-2001</h3>
+                <p class="text-sm ubuntu-sans-mono-code leading-snug tracking-wide text-white text-opacity-100"><span class="font-bold">Carrier Transicold de México</span> - Capturista de datos</p>
+              </div>
+            </div>
+    
+            <!-- Evento 2 -->
+            <div class="mb-8 flex justify-between items-center w-full">
+              <div class="order-1 w-5/12 soft-glass-ui rounded-lg shadow-xl px-6 py-4">
+                <h3 class="mb-3 ubuntu-sans-mono-code font-bold text-white text-xl">2001-2012</h3>
+                <p class="text-sm ubuntu-sans-mono-code leading-snug tracking-wide text-white text-opacity-100"><span class="font-bold">Grupo sur promociones</span> - Capturista de datos, Analista programador, Jefe de Sistemas.</p>
+              </div>
+              <div class="z-20 flex items-center order-1 soft-glass-ui shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">2</h1>
+              </div>
+              <div class="order-1 w-5/12"></div>
+            </div>
+    
+            <!-- Evento 3 -->
+            <div class="mb-8 flex justify-between items-center w-full">
+              <div class="order-1 w-5/12"></div>
+              <div class="z-20 flex items-center order-1 soft-glass-ui shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">3</h1>
+              </div>
+              <div class="order-1 soft-glass-ui rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-3 font-bold text-white text-xl">2012-2016</h3>
+                <p class="text-sm leading-snug tracking-wide text-white text-opacity-100">Descripción del evento 3.</p>
+              </div>
+            </div>
+    
+            <!-- Evento 4 -->
+            <div class="mb-8 flex justify-between items-center w-full">
+              <div class="order-1 w-5/12 soft-glass-ui rounded-lg shadow-xl px-6 py-4">
+                <h3 class="mb-3 font-bold text-white text-xl">2016-2018</h3>
+                <p class="text-sm leading-snug tracking-wide text-white text-opacity-100">Descripción del evento 4.</p>
+              </div>
+              <div class="z-20 flex items-center order-1 soft-glass-ui shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">4</h1>
+              </div>
+              <div class="order-1 w-5/12"></div>
+            </div>
+
+            <!-- Evento 5 -->
+            <div class="mb-8 flex justify-between items-center w-full">
+              <div class="order-1 w-5/12"></div>
+              <div class="z-20 flex items-center order-1 soft-glass-ui shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">5</h1>
+              </div>
+              <div class="order-1 soft-glass-ui rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-3 font-bold text-white text-xl">2018-2022</h3>
+                <p class="text-sm leading-snug tracking-wide text-white text-opacity-100">Descripción del evento 3.</p>
+              </div>
+            </div>
+    
+            <!-- Evento 6 -->
+            <div class="mb-8 flex justify-between items-center w-full">
+              <div class="order-1 w-5/12 soft-glass-ui rounded-lg shadow-xl px-6 py-4">
+                <h3 class="mb-3 font-bold text-white text-xl">2022-2023</h3>
+                <p class="text-sm leading-snug tracking-wide text-white text-opacity-100">Descripción del evento 4.</p>
+              </div>
+              <div class="z-20 flex items-center order-1 soft-glass-ui shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">6</h1>
+              </div>
+              <div class="order-1 w-5/12"></div>
+            </div>
+
+            <!-- Evento 7 -->
+            <div class="mb-8 flex justify-between items-center w-full">
+              <div class="order-1 w-5/12"></div>
+              <div class="z-20 flex items-center order-1 soft-glass-ui shadow-xl w-8 h-8 rounded-full">
+                <h1 class="mx-auto font-semibold text-lg text-white">7</h1>
+              </div>
+              <div class="order-1 soft-glass-ui rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 class="mb-3 font-bold text-white text-xl">2024</h3>
+                <p class="text-sm leading-snug tracking-wide text-white text-opacity-100">Descripción del evento 3.</p>
+              </div>
+            </div>            
+
+          </div>
         </div>
       </div>
       <!-- Aquí puedes añadir tus proyectos -->
--- src/components/pages/home/myname.js
+++ src/components/pages/home/myname.js
@@ -1,19 +1,19 @@
 export function loadMyName() {
     const content = /* html */
-    `<section id="inicio" class="flex items-center justify-center h-screen z-10">
-      <div class="text-center">
-          <!-- Internal Links -->
-        <div class="space-x-4">
-          <a href="#experiencia" class="ubuntu-sans-mono-code text-gray-300 hover:text-white">&lt;experiencia/&gt;</a>
-          <a href="#projects" class="ubuntu-sans-mono-code text-gray-300 hover:text-white">&lt;proyectos/&gt;</a>
-          <a href="#contact" class="ubuntu-sans-mono-code text-gray-300 hover:text-white">&lt;contacto/&gt;</a>
-        </div>
-      
-        <h1 class="text-[65px] font-bold montserrat-alternates-semibold">René González Campos XD</h1>
-        <p class="mt-4 text-[30px] ubuntu-sans-mono-code2 text-xl">Programador Senior Backend y Frontend</p>
-        <p class="mt-4 text-[18px]  ubuntu-sans-mono-code text-sm text-gray-500">Desarrollo experiancias digitales al pixel, accesibles y atractivas.</p>
+    `<section id="inicio" class="flex items-center justify-center h-screen">
+      <div class="text-center">     
+        <h1 class="text-[55px] font-bold montserrat-alternates-semibold">René González Campos</h1>
+        <p class="mt-4 ubuntu-sans-mono-code text-3xl">Programador Senior</p>
+        <p class="mt-4 text-[18px] ubuntu-sans-mono-code text-sm text-gray-500 hidden md:block ">Desarrollo experiancias digitales al pixel, accesibles y atractivas.</p>
       </div>
     </section>
+
+    <!-- Internal Links -->
+    <div class="absolute top-2/3 flex item-center justify-center space-x-4 z-10" style="width:100%;">
+      <a href="#experiencia" class="ubuntu-sans-mono-code text-gray-300 hover:text-white z-20">&lt;experiencia/&gt;</a>
+      <a href="#projects" class="ubuntu-sans-mono-code text-gray-300 hover:text-white">&lt;proyectos/&gt;</a>
+      <a href="#contact" class="ubuntu-sans-mono-code text-gray-300 hover:text-white">&lt;contacto/&gt;</a>
+    </div>
     `
 
     return content
